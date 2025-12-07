3383c700437d3e81fcb50c89bf893f136d66248f
Damien <117820941+Damien-Test-Dev@users.noreply.github.com>
2025-12-07 20:01:39 +0100

    Create app.js
    start

---
+++ app.js
@@ -0,0 +1,6 @@
+// app.js
+// Sprint 1 : rien de fonctionnel, juste un point d'entrée propre.
+// On utilisera ce fichier au Sprint 2 pour afficher plusieurs cartes,
+// gérer la navigation, et plus tard le stockage local.
+
+console.log("Flashcards iPad - app.js chargé");
